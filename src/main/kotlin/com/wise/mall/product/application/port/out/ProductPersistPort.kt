806dda9e3c04ff29aeb13f757f2a023fd9d96b96
tableminpark <tablemin_park@daum.net>
2025-04-06 22:58:21 +0900

    fix: 도메인 구조 수정
    - 상품 등록 Vo, 상품 수정 Vo 생성

--- src/main/kotlin/com/wise/mall/product/application/port/out/ProductPersistPort.kt
+++ src/main/kotlin/com/wise/mall/product/application/port/out/ProductPersistPort.kt
@@ -1,10 +1,11 @@
 package com.wise.mall.product.application.port.out
 
-import com.wise.mall.product.application.domain.model.Product
+import com.wise.mall.product.application.vo.CreateProductVo
+import com.wise.mall.product.application.vo.UpdateProductVo
 
 interface ProductPersistPort {
 
-    fun createProduct(product: Product)
+    fun createProduct(createProductVo: CreateProductVo)
 
-    fun updateProduct(product: Product)
+    fun updateProduct(updateProductVo: UpdateProductVo)
 }
