71750c2656700465750456bd93723687abd9c8a3
Danesnote <tyxmfh@naver.com>
2025-04-11 11:37:14 +0900

    시뮬레이션 부분 삭제

--- src/main/kotlin/com/wise/mall/delivery/adapter/in/web/dto/request/simulation/RegistShippingRequestDto.kt
+++
@@ -1,5 +0,0 @@
-package com.wise.mall.delivery.adapter.`in`.web.dto.request.simulation
-
-data class RegistShippingRequestDto(
-    val orderId: String
-)
--- src/main/kotlin/com/wise/mall/delivery/adapter/in/web/dto/request/simulation/UpdateShippingRequestDto.kt
+++
@@ -1,6 +0,0 @@
-package com.wise.mall.delivery.adapter.`in`.web.dto.request.simulation
-
-data class UpdateShippingRequestDto(
-    val trackingNumber: String,
-    val status: String
-)
