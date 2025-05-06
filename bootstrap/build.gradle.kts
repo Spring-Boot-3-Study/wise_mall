2e6d2e3a5f1570c09700cd26cbf6c3350f35e1fe
Hong Seok In <hrpt8@naver.com>
2025-05-06 09:22:26 +0900

    bootstrap spring-starter 추가

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
 
 dependencies {
 	implementation(project(":adapter-in"))
+	implementation("org.springframework.boot:spring-boot-starter")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
