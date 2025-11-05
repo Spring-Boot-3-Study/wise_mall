4060c4354f5171ed7fe25d87c7bc64ff4de634e3
jinho <jinho@jinhoui-MacBookPro.local>
2025-11-05 15:41:34 +0900

    chore: configure flyway with initial schema

--- wise_mall_monolithic/src/main/kotlin/com/wisemall/monolithic/WiseMallMonolithicApplication.kt
+++ wise_mall_monolithic/src/main/kotlin/com/wisemall/monolithic/WiseMallMonolithicApplication.kt
@@ -2,6 +2,7 @@ package com.wisemall.monolithic
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 class WiseMallMonolithicApplication
