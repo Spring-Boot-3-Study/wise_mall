76f64d02d8e793d108b5410080ebbbf74f879684
jinho <jinho@jinhoui-MacBookPro.local>
2025-11-05 15:55:59 +0900

    chore: gradle관련 gitignore 추가 및 readme.md 수정

--- wise_mall_monolithic/src/main/kotlin/com/wisemall/monolithic/v1/payment/api/PaymentController.kt
+++ wise_mall_monolithic/src/main/kotlin/com/wisemall/monolithic/v1/payment/api/PaymentController.kt
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/v1/payment")
+@RestController
+@RequestMapping("/api/v1/payment")
 class PaymentController(
     private val paymentService: PaymentService
 ) {
